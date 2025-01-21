35e6f0997b80e488d9735ed05cd5e5dc60346c83
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2025-01-22 03:04:57 +0600

    update: include ViewToggle and Search input

--- components/menu-bar.tsx
+++ components/menu-bar.tsx
@@ -1,11 +1,24 @@
+/**
+ * FILE: components/menu-bar.tsx
+ * DESC: A menu bar component that contains the view toggle and theme toggle.
+ */
+
 import React from "react";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { ViewToggle } from "@/components/design/ViewToggle";
+import { SearchInput } from "@/components/design/SearchInput";
 
 export const MenuBar = () => {
     return (
-        <section className="flex items-center justify-between p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 2xl:p-14">
-            <div></div>
-            <div>
+        <section className="flex items-center justify-between p-4 sm:p-6 lg:p-8 2xl:p-12">
+            <div className="flex items-center space-x-4">
+                {/* View Options */}
+                <ViewToggle />
+            </div>
+            <div className="flex items-center space-x-4">
+                {/* Search Input */}
+                <SearchInput />
+                {/* Theme Toggle Selector */}
                 <ThemeToggle />
             </div>
         </section>
