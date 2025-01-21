b8ce8e6b9513fb8ee9e0d4123c77638d63bc1a39
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2025-01-22 03:02:40 +0600

    feat: implement main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+/**
+ * FILE: app/page.tsx
+ * DESC: This file exports the main page component that displays the names of Allah based on the view type.
+ */
+
+import { namesOfAllah } from "@/lib/data/names";
+import Manager from "@/components/design/Manager";
+import { RawTable } from "@/components/design/ListView";
 
 
 export default function Home() {
-  return (
-    <>
-      <main>
 
-      </main>
-    </>
+  return (
+    <main className="min-h-screen w-full md:p-8">
+      <RawTable names={namesOfAllah} className="sr-only" /> {/* This is for screen readers */}
+      <Manager />
+    </main>
   );
 }
